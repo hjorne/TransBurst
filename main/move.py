681bec95acbc2c7ca32f8b4a057654a88bc81f27
hjorne <joeh19@gmail.com>
2015-08-20 13:35:51 -0400

    Added docstrings to move.py

--- main/move.py
+++ main/move.py
@@ -1,33 +1,51 @@
+""" Small collection of functions about moving data to and from swift """
 from threading import Thread
 
 
 def move_thread(swift_client, clip, container):
-    print clip
+    """ Thread that gets called so swift moves can be done in parallel """
+    print 'Uploading %s' % clip
     with open(clip, 'rb') as f:
         swift_client.put_object(container, clip, contents=f,
                                 content_type="video")
-    print "Done uploading %s" % clip
+    print 'Done uploading %s' % clip
 
 
 def move(swift_client, file_list, container="videos"):
+    """ Moves a list of files in 'parallel' using threads into a swift
+    container
+    """
     swift_client.put_container(container)
-    print "\"Videos\" container created"
+    print '\"Videos\" container created'
     threads = []
+    # Loops through all the video clips in the file_list to upload them to swift
     for clip in file_list:
-        threads.append(
-            Thread(target=move_thread, args=(swift_client, clip, container)))
+        arg_list = (swift_client, clip, container)
+
+        # Here we keep a list of the threads moving stuff to swift
+        threads.append(Thread(target=move_thread, args=arg_list).start())
         threads[-1].start()
+
+    # This ensures that the move function will not exit until all threads are
+    # finished
     for thread in threads:
         thread.join()
-    print "Done uploading to LOCAL cloud..."
+
+    print 'Done uploading to LOCAL cloud...'
 
 
 def retrieve(swift_client):
+    """ Grabs all of the files from the 'completed' folder in the the local
+    swift, which is where they are always put at the end of a transcode
+    """
     container_data = []
-    for data in swift_client.get_container("completed")[1]:
+    # Goes through to get all of the files in the container
+    for data in swift_client.get_container('completed')[1]:
         container_data.append('{0}'.format(data['name']))
+
+    # Goes through to download all of the files found in the container
     for f in container_data:
-        print "Downloading %s to local drive..." % f
+        print 'Downloading %s to local drive...' % f
         obj_tuple = swift_client.get_object('completed', f)
         with open(f, 'wb') as xcode_bytes:
             xcode_bytes.write(obj_tuple[1])
